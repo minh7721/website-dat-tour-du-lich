c9f521c6457a9b07309e29e6fe6788b2edf0f6a1
minh7721 <nhatminh7721@gmail.com>
2022-05-30 09:34:17 +0700

    login

--- login.php
+++ login.php
@@ -1,18 +1,32 @@
+<?php
+$HOST = 'localhost';
+$USERNAME = 'root';
+$DB_NAME = 'travel';
+$PASSWORD = '';
+$conn = new PDO("mysql:host=" . $HOST . ";dbname=" .$DB_NAME . ";charset=utf8", $USERNAME,$PASSWORD);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+if(!$conn){
+	echo "Connect failed";
+}
+?>
+
 <!doctype html>
 <html lang="en">
-  <head>
-  	<title>Đăng nhập</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<head>
+	<title>Đăng nhập</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	
+
 	<link rel="stylesheet" href="css/style.css">
 
-	</head>
-	<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+</head>
+
+<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
 	<section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
@@ -23,46 +37,46 @@
 			<div class="row justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<div class="login-wrap p-0">
-		      	<form action="#" class="signin-form">
-		      		<div class="form-group">
-		      			<input type="text" class="form-control" placeholder="Username" required>
-		      		</div>
-	            <div class="form-group">
-	              <input id="password-field" type="password" class="form-control" placeholder="Password" required>
-	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3">Đăng nhập</button>
-	            </div>
-	            <div class="form-group d-md-flex">
-	            	<div class="w-50">
-		            	<label class="checkbox-wrap checkbox-primary">Nhớ tôi
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
+						<form action="#" class="signin-form" method="POST">
+							<div class="form-group">
+								<input name="email" type="text" class="form-control" placeholder="Email" required>
+							</div>
+							<div class="form-group">
+								<input name="pass" id="password-field" type="password" class="form-control" placeholder="Password" required>
+								<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+							</div>
+							<div class="form-group">
+								<button name="btnLogin" type="submit" class="form-control btn btn-primary submit px-3">Đăng nhập</button>
+							</div>
+							<div class="form-group d-md-flex">
+								<div class="w-50">
+									<label class="checkbox-wrap checkbox-primary">Nhớ tôi
+										<input type="checkbox" checked>
+										<span class="checkmark"></span>
 									</label>
 								</div>
 								<div class="w-50 text-md-right">
 									<a href="#" style="color: #fff">Quên mật khẩu</a>
 								</div>
-	            </div>
-	          </form>
-              <div class="text-center">Bạn chưa có tài khoản? <a href="./registration.php">dăng ký ngay</a></div>
-	          <p class="w-100 text-center">&mdash; hoặc đăng nhập bằng &mdash;</p>
-	          <div class="social d-flex text-center">
-	          	<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
-	          	<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Gmail</a>
-	          </div>
-		      </div>
+							</div>
+						</form>
+						<div class="text-center">Bạn chưa có tài khoản? <a href="./registration.php">dăng ký ngay</a></div>
+						<p class="w-100 text-center">&mdash; hoặc đăng nhập bằng &mdash;</p>
+						<div class="social d-flex text-center">
+							<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> Facebook</a>
+							<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span> Gmail</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
 	<script src="js/jquery.min.js"></script>
-  <script src="js/popper.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+	<script src="js/popper.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/main.js"></script>
 
-	</body>
+</body>
+
 </html>
-
